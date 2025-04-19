dfb6953c7f6cd422ea1c024fd58464f186e66856
mahdi1382as <96875467+mahdi1382as@users.noreply.github.com>
2025-04-19 06:31:14 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-# بارگیری مقادیر از متغیرهای محیطی یا استفاده از مقادیر پیش‌فرض
-API_ID = os.getenv("API_ID", "your_api_id_here")
-API_HASH = os.getenv("API_HASH", "your_api_hash_here")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
-SESSION_NAME = os.getenv("SESSION_NAME", "default_session_name")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
