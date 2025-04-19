3ce367c7ad14422776ec4706d2a166d840739553
mahdi1382as <96875467+mahdi1382as@users.noreply.github.com>
2025-04-19 06:00:42 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
-# تنظیمات تلگرام
+# متغیرهای API و TOKEN از Railway یا محیط‌های دیگر (یا به صورت دستی)
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# اگر نیاز به یک session name دارید، باید این متغیر را تعریف کنید
+SESSION_NAME = os.getenv("SESSION_NAME", "default_session_name")
