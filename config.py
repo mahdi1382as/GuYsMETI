b4827b9ce03f2a75c74dc442d93322fe695247b7
mahdi1382as <96875467+mahdi1382as@users.noreply.github.com>
2025-04-19 06:05:27 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # متغیرهای API و TOKEN از Railway یا محیط‌های دیگر (یا به صورت دستی)
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("773349916:AAEhxMKH2yOH6oqu5OPLZ-M2LM9qnwvzFFI")
 
 # اگر نیاز به یک session name دارید، باید این متغیر را تعریف کنید
 SESSION_NAME = os.getenv("SESSION_NAME", "default_session_name")
